8d1c5302e4c06dff5e5acf3390d2b07d7bb5f86c
wangyonghui <2930807240@qq.com>
2020-08-25 14:13:32 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/DynamicDatasourceApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApplication.class, args);
+    }
+
+}
